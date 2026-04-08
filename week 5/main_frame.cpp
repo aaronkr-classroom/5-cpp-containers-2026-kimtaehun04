9beade763ed7c40304855599d3f631898d1b917e
ththfql <145094239+ththfql@users.noreply.github.com>
2026-04-08 13:45:31 +0900

    homework

--- week 5/main_frame.cpp
+++ week 5/main_frame.cpp
@@ -7,16 +7,10 @@
 using namespace std;
 
 int main() {
-	string s;
 	vector<string> v;
-	//문자열을 한 행씩 입력받아 벡터에 저장
-	while (getline(cin, s)) {
-		v.push_back(s);
-	}
-	//벡터 v에 저장한 문자열을 프레임으로 감싸서 출력
-	vector<string> f = frame(v);
-	for (vector<string>::size_type i = 0; i != f.size(); i++) {
-		cout << f[i] << endl;
-	}
+	cout << "Enter any string, EOF to quit: " << endl;
+
+	while(getline)
+	
 	return 0;
 }
